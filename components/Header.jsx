eaa42096eb9187d792e79c0f5fada264b83e25b7
alexaldearroyo <alexaldearroyo@gmail.com>
2023-11-25 01:14:42 +0900

    headerNav layout set

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import styles from '../styles/components/Header.module.css';
-
+import React from "react";
+import styles from "../styles/components/Header.module.css";
 
 function Header() {
   return (
     <div className={styles.header}>
-      <div className={styles.headerText}>HeaderText</div>
-      <div className={styles.headerNav}>HeaderNav</div>
+      <div className={styles.headerText}>
+        <p>Alex Arroyo</p>
+      </div>
+      <div className={styles.headerNav}>
+        <a href="#" className={styles.navLink}>
+          Home
+        </a>
+        <a href="#" className={styles.navLink}>
+          Artworks
+        </a>
+        <a href="#" className={styles.navLink}>
+          More
+        </a>
+      </div>
     </div>
   );
 }
