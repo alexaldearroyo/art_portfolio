b10dced9b410b4092cead2bc9a7f29493c19da8c
alexaldearroyo <alexaldearroyo@gmail.com>
2023-11-25 00:51:15 +0900

    index layout improved

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import styles from '../styles/components/Header.module.css';
+
 
 function Header() {
   return (
-    <>
-      Header
-    </>
+    <div className={styles.header}>
+      <div className={styles.headerText}>HeaderText</div>
+      <div className={styles.headerNav}>HeaderNav</div>
+    </div>
   );
 }
 
