80157cf2c345c9e2883d897368b0466b4d6732ee
alexaldearroyo <alexaldearroyo@gmail.com>
2024-01-10 03:03:06 +0900

    Build made

--- components/ImageTest.jsx
+++ components/ImageTest.jsx
@@ -4,8 +4,8 @@ import Image from "next/image"
 function ImageTest() {
   return (
     <div>
-      <h1>Prueba de Imagen</h1>
-      <Image src="/001.png" width="64" height="64" />
+      <h1>Image Test</h1>
+      <Image src="/001.png" alt="Image Test" width="64" height="64" />
     </div>
   );
 }
--- pages/more.js
+++ pages/more.js
@@ -9,7 +9,7 @@ export default function Home() {
     <>
         <Main>
         <p>This is the digital art portfolio of Alex Arroyo.</p>
-        <p>For more information about the author's work,</p>
+        <p>For more information about the author&apos;s work,</p>
         <p>access his <a href="https://alexaldearroyo.github.io/portfolio" target="_blank" rel="noopener noreferrer" style={{ color: '#FD7C65', textDecoration: 'underline' }}>developer portfolio</a>.</p>
 
         </Main>
